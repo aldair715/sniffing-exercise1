9d23046b505b09ffef5c5a778f7f5e6bd33e73d0
Aldair <aldapanuni@gmail.com>
2024-01-16 20:27:55 -0400

    deleted:    Helpers/HTTP_CLIENT.js 	deleted:    Helpers/HTTP_ERROR.js 	deleted:    Helpers/WEB_SERVER.js

--- Helpers/WEB_SERVER.js
+++
@@ -1,6 +0,0 @@
-const WEB_SERVER = (req, res, htmlCode) => {
-  res.statusCode = 200;
-  res.setHeader("Content-type", "text/html;charset=utf-8");
-  res.end(htmlCode);
-};
-export default WEB_SERVER;
